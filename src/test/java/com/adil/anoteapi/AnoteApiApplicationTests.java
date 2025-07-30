e87614e980f2caf45d7dcb0afe080fa4f442ec05
ibadovadil <adilibadov654@gmail.com>
2025-07-30 14:45:58 +0400

    inital commit

---
+++ src/test/java/com/adil/anoteapi/AnoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adil.anoteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
